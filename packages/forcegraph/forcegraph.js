fb7bfb776912db39c0d8d4a16e25d76ebd64e184
Damiaan Dufaux <info@devian.tk>
2016-09-06 14:13:50 +0200

    add standard viewbox

--- packages/forcegraph/forcegraph.js
+++ packages/forcegraph/forcegraph.js
@@ -8,9 +8,7 @@ Template.ForceGraph.onRendered(function() {
 	const links = [];
 
 	const svg = d3.select(this.find('svg'))
-		.attr('width', 500)
-		.attr('height', 500);
-
+		.attr('viewBox', '0 0 500 500');
 
 	const force = d3.layout.force()
 		.nodes(nodes)
